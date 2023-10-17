9d550307baba3c335e3933e5ad7b54f46949c628
hsilents <1024968781@qq.com>
2023-10-17 22:51:15 +0900

    load/save img v1

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,98 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+    static ImageView imgView;
+    String imageUrl = "https://kiokahn.synology.me:30000/";
+    Bitmap bmImg = null;
+    CLoadImage task;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imgView = findViewById(R.id.imgView);
+        task = new CLoadImage();
+    }
+
+    public void onClickForLoad(View v) {
+        task.execute("https://i.postimg.cc/7ZRnHnR9/Gazzi-Labs-CI-type-B-big-logo.png");
+        Toast.makeText(getApplicationContext(), "Load", Toast.LENGTH_LONG).show();
+    }
+
+    public void onClickForSave(View v) {
+        saveBitmaptoJpeg(bmImg, "DCIM", "image");
+        Toast.makeText(getApplicationContext(), "Save", Toast.LENGTH_LONG).show();
+    }
+
+    private static class CLoadImage extends AsyncTask<String, Integer, Bitmap> {
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+            try {
+                URL myFileUrl = new URL(urls[0]);
+                HttpURLConnection conn = (HttpURLConnection) myFileUrl.openConnection();
+                conn.setDoInput(true);
+                conn.connect();
+
+                InputStream is = conn.getInputStream();
+                return BitmapFactory.decodeStream(is);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        protected void onPostExecute(Bitmap img) {
+            if (img != null) {
+                imgView.setImageBitmap(img);
+            } else {
+
+            }
+        }
+    }
+
+    public static void saveBitmaptoJpeg(Bitmap bitmap, String folder, String name) {
+        String ex_storage = Environment.getExternalStorageDirectory().getAbsolutePath();
+        String folder_name = "/" + folder + "/";
+        String file_name = name + ".jpg";
+        String string_path = ex_storage + folder_name;
+        Log.d("路径", string_path);
+
+        File file_path;
+        file_path = new File(string_path);
+
+        if (!file_path.exists()) {
+            file_path.mkdirs();
+        }
+
+        try {
+            FileOutputStream out = new FileOutputStream(string_path + file_name);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+            out.close();
+        } catch (FileNotFoundException exception) {
+            Log.e("FileNotFoundException", exception.getMessage());
+        } catch (IOException exception) {
+            Log.e("IOException", exception.getMessage());
+        }
     }
 }
+
